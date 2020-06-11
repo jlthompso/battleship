f4a3ec750475bd7f06584216e6080c2290a9f886
Joe Thompson <jthompso@mtu.edu>
2020-06-10 23:27:23 -0700

    Updates

---
+++ precache-manifest.0a7a6ce196a9f187930dd9289011343e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a4c78774b299c376b51ddf264ff41b5a",
+    "url": "/battleship/index.html"
+  },
+  {
+    "revision": "d986bab10ba47bd16543",
+    "url": "/battleship/static/css/main.9b9760c9.chunk.css"
+  },
+  {
+    "revision": "18260e7e9b7d1cad6539",
+    "url": "/battleship/static/js/2.421190ff.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/battleship/static/js/2.421190ff.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d986bab10ba47bd16543",
+    "url": "/battleship/static/js/main.5aad8864.chunk.js"
+  },
+  {
+    "revision": "fa2bb6d32141478767f3",
+    "url": "/battleship/static/js/runtime-main.ceb60dc6.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/battleship/static/media/logo.5d5d9eef.svg"
+  }
+]);
